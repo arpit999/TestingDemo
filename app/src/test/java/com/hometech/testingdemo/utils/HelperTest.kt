7feafdb95101b215c7188fea1811dd2fb9f25e95
arpit999 <arpitpatel009@gmail.com>
2023-01-07 15:28:43 -0500

    Created HelperTest.kt in test folder and define require steps for all test.
    1) Arrange : Ready code for test
    2) Act : Perform necessary actions to ready code
    3) Assert : Add actual & expected input to verify

---
+++ app/src/test/java/com/hometech/testingdemo/utils/HelperTest.kt
@@ -0,0 +1,14 @@
+package com.hometech.testingdemo.utils
+
+
+import org.junit.Test
+
+class HelperTest {
+
+    @Test
+    fun isPallindrome() {
+        //Arrange
+        //Act
+        //Assert
+    }
+}
