689a00f1424b82148861018f1c0dedc50574006e
arpit999 <arpitpatel009@gmail.com>
2023-01-07 15:13:05 -0500

    Initial commit

---
+++ app/src/main/java/com/hometech/testingdemo/utils/Helper.kt
@@ -0,0 +1,21 @@
+package com.hometech.testingdemo.utils
+
+class Helper {
+
+    fun isPallindrome(inputString: String): Boolean {
+
+        var i = 0
+        var j = inputString.length - 1
+        var result = true
+
+        while (i < j) {
+            if (inputString[i] != inputString[j]) {
+                result = false
+                break
+            }
+            i++
+            j++
+        }
+        return result
+    }
+}
---
+++ app/src/test/java/com/hometech/testingdemo/utils/HelperTest.kt
@@ -0,0 +1,15 @@
+package com.hometech.testingdemo.utils
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class HelperTest {
+
+    @Test
+    fun isPallindrome() {
+        //Arrange
+        //Act
+        //Assert
+    }
+}
